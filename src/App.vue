646384627fe2218313f8f9f3c0e6443c5ff99114
yang20002020 <2628636987@qq.com>
2023-09-05 15:09:19 +0800

    vue  开发流程

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">vue Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div >
+   {{message}}
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      message:"hello vue222"
+    }
+  },
+
+  methods: {
+    
+  }
+ }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
