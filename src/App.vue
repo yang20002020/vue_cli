b2dce922f74d5aa8c02a5b9b43188b19fe227fe1
yang20002020 <2628636987@qq.com>
2023-09-06 14:24:53 +0800

    Element 快速入门

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div >
-   {{message}}
+    {{message}}
+   <element-view></element-view>
   </div>
 </template>
 
 <script>
+import ElementView from "@/views/element/ElementView";
 export default {
+  components:{ElementView},
   data() {
     return {
       message:"hello vue222"
