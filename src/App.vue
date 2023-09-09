08ccc1155046a2c1652f2a90ecc22a5d3cb315b9
yang20002020 <2628636987@qq.com>
2023-09-09 10:01:37 +0800

    Element-案例-vue 路由

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <!-- {{message}} -->
     <!-- <element-view></element-view> -->
-    <emp-view></emp-view>
+<!--    员工管理-->
+<!--    <emp-view></emp-view>-->
+<!--   动态展示组件-->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // import ElementView from "@/views/element/ElementView";
- import EmpView from "./views/tlias/EmpView.vue";
+//  import EmpView from "./views/tlias/EmpView.vue";
 export default {
   // components: { ElementView },
-  components: { EmpView },
+  // components: { EmpView },
   data() {
     return {
       message: "hello vue222",
